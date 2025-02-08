77511f01a3c60b3e4cf2d6ca0f1d44b26b5b01c3
zhoulj <994817556@qq.com>
2025-02-08 18:30:52 +0800

    modify routes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,5 +1,5 @@
 # rushi_login/app/auth/routes.py
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user
 from app.models import User
 from .forms import LoginForm, RegistrationForm  # 新增表单类
@@ -8,14 +8,14 @@ from . import bp  # 导入蓝图对象
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('main.welcome'))
+        return redirect(url_for('auth.welcome'))
 
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and user.verify_password(form.password.data):
             login_user(user)
-            return redirect(url_for('main.welcome'))
+            return redirect(url_for('auth.welcome'))
         flash('用户名或密码错误')
     return render_template('login.html', form=form)
 
